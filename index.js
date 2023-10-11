f1e8153a158c04f97d856ad37aaec009943d0fe7
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-10-11 06:52:07 -0700

    about me page

---
+++ index.js
@@ -0,0 +1,19 @@
+// about
+ // getting the skills set
+ const titleDescs = document.getElementsByClassName('title_desc');
+ const titleSetContents = document.getElementsByClassName('title_set_content');
+
+ // using a function and for of loop to loop through the content when clicked
+ function setTitle(contents){
+     // removes the active page when new page is clicked
+     for(titleDesc of titleDescs){
+         titleDesc.classList.remove('active');
+     }
+     
+     for(titleSetContent of titleSetContents){
+         titleSetContent.classList.remove('first_content');
+     }
+     // adding event to add content to active page
+     event.currentTarget.classList.add('active');
+     document.getElementById(contents).classList.add('first_content');
+ }
