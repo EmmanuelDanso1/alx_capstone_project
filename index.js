97302a04a90c1f432c67ac517443537a8eb2379a
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-10-17 13:12:12 -0700

    js section for form validation, media queries

--- index.js
+++ index.js
@@ -28,3 +28,74 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right = '-13rem';
 }
+
+// form validation
+
+function validateForm() {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+
+    const nameError = document.getElementById('name-error');
+    const emailError = document.getElementById('email-error');
+    const messageError = document.getElementById('message-error');
+    const successMessage = document.getElementById('success-message');
+
+    // Clear previous error messages
+    nameError.textContent = '';
+    emailError.textContent = '';
+    messageError.textContent = '';
+
+    let isValid = true;
+
+    if (name.value.trim() === '') {
+        nameError.textContent = 'Name is required';
+        isValid = false;
+    }
+
+    if (email.value.trim() === '') {
+        emailError.textContent = 'Email is required';
+        isValid = false;
+    } else if (!validateEmail(email.value)) {
+        emailError.textContent = 'Invalid email format';
+        isValid = false;
+    }
+
+    if (message.value.trim() === '') {
+        messageError.textContent = 'Message is required';
+        isValid = false;
+    }
+
+    if (isValid) {
+        successMessage.innerHTML = 'Form submitted successfully!'
+        setInterval(function(){
+            successMessage.innerHTML = '';
+        }, 7000)
+        form.reset();
+        successMessage.style.display = 'block';
+        return false; // Prevent actual form submission for this example
+    } else {
+        successMessage.style.display = 'none';
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+// using emailjs to configure email when visitors check the site
+function sendMail(){
+let serviceID = "service_nshb4v5";
+let templateID = "template_3mp2ie4";
+
+let data = {
+    senderName: document.querySelector("#name").value,
+    senderEmail: document.querySelector("#email").value,
+    senderMessage : document.querySelector("#message").value
+};
+emailjs.send(serviceID,templateID,data).then(
+
+).catch((error) =>console.log(error)  )
+}
